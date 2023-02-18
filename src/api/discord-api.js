6eaa0f6e40f5c74a35460107e6490a51d8217ae4
Bertrand Dancot <b.dancot@gmail.com>
2023-02-18 17:34:32 +0100

    remove + in string [abs]

--- src/api/discord-api.js
+++ src/api/discord-api.js
@@ -5,11 +5,12 @@ const USER_ENDPOINT = "users/";
 const GUILD_ENDPOINT = `/guilds/${GUILD_ID}/`;
 
 export async function getUserById(id) {
-    return await DiscordRequest(DISCORD_API + USER_ENDPOINT + id, { method: 'GET' });
+    return await DiscordRequest(`${DISCORD_API}${USER_ENDPOINT}${id}`, { method: 'GET' });
 }
 
 export async function getGuildUserById(id) {
-    // return await DiscordRequest(DISCORD_API + GUILD_ENDPOINT + "members/" + id, { method: 'GET' });
+    // return await DiscordRequest(`${DISCORD_API}${GUILD_ENDPOINT}members/${id}`, { method: 'GET' });
+    // Temporary to not hit the rate limit
     return await (new Promise((resolve, reject) => {
         resolve({
             "avatar": null,
