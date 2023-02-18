da2a1e3d0b4c72b3056382d2d74f6a78eedec19a
Bertrand Dancot <b.dancot@gmail.com>
2023-02-18 17:29:19 +0100

    Abstraction Level up [abs]

--- src/utils/lottery.js
+++ src/utils/lottery.js
@@ -1,7 +1,19 @@
 import { getGuildUserById } from "../api/discord-api.js";
-import { EMBEDS_COLOR } from "../constants/app-constants.js";
+import { EMBEDS_COLOR, KEYV_LOTTERIES_PREFIX } from "../constants/app-constants.js";
+import { getXRandomItemsFromArray } from "./array.js";
 import { buildAvatarUrl } from "./discord-tools.js";
 
+// Could do some validation but.. :)
+// Make sure the total of each array equal to 1
+const WINNING_DISTRIBUTION = [
+    [1],
+    [0.6, 0.4],
+    [0.5, 0.3, 0.2]
+];
+
+export function createLotteriesName(guildId, userId) {
+    return `${KEYV_LOTTERIES_PREFIX}_${guildId}_${userId}`;
+}
 export function hasActiveLottery(lotteries) {
     let res = null;
     [...lotteries].forEach(lottery => {
@@ -10,17 +22,55 @@ export function hasActiveLottery(lotteries) {
     return res;
 }
 
-export function updateLotteries(lotteries, newLottery) {
-    return [...lotteries].map(lottery => {
-        if (lottery.active) return newLottery;
+export function isAllowedToUpdate(lottery, userId) {
+    return lottery.owner === userId || lottery.allowedUsers.includes(userId);
+}
+export function isAllowedIn(lottery, userId) {
+    return lottery.allowedUsers.includes(userId);
+}
+
+export function isPlayerIn(lottery, playerId) {
+    return lottery.players.includes(playerId);
+}
+
+export function replaceActiveLottery(lotteries, newLottery) {
+    let done = false;
+    const updatedLotteries = [...lotteries].map(lottery => {
+        if (lottery.active) {
+            done = true;
+            return newLottery;
+        }
         return lottery;
     });
+
+    if (!done) {
+        throw new Error("there is no active Lottery");
+    }
+
+    return updatedLotteries;
+}
+
+export function handleWinnersLottery(lottery, podiumSize, taxPercent) {
+    const realPodiumSize = Math.min(lottery.players.length, podiumSize);
+    const totalAmount = lottery.price * lottery.players.length;
+    const amountTax = totalAmount * taxPercent / 100;
+    const amountWinners = totalAmount - amountTax;
+    const winners = getXRandomItemsFromArray(lottery.players, realPodiumSize);
+    let message = `${winners.length ? "Congratulations to" : ""}`;
+    const podium = winners.map((player, index, array) => {
+        message += ` #${index + 1} <@${player}>`;
+        return {
+            id: player,
+            amount: amountWinners * WINNING_DISTRIBUTION[array.length - 1][index]
+        };
+    });
+
+    return { podium, amountTax, message };
 }
 
 // Embeds
 export async function buildEmbedsLottery(lottery) {
     const owner = await getGuildUserById(lottery.owner);
-
     const createdFields = await Promise.all(lottery.players.map(async (playerId) => {
         const playerGuilds = await getGuildUserById(playerId);
         return {
@@ -56,7 +106,6 @@ export async function buildEmbedsLottery(lottery) {
 
 export async function buildEmbedsWinnersLottery(lottery, podium, amountTax) {
     const owner = await getGuildUserById(lottery.owner);
-
     const createdFields = await Promise.all(podium.map(async (player, index) => {
         const playerGuilds = await getGuildUserById(player.id);
         return {
@@ -95,4 +144,18 @@ export async function buildEmbedsWinnersLottery(lottery, podium, amountTax) {
         //     icon_url: 'https://i.imgur.com/aVq1dRh.png',
         // }
     };
+}
+
+export async function doAndAnswer(action, baseErrorMessage) {
+    let answer;
+    try {
+        const res = await action();
+        answer = {
+            content: res.message,
+            ...(res.data && { embeds: [res.data] })
+        };
+    } catch (e) {
+        answer = `${baseErrorMessage}, ${e.message}`;
+    }
+    return answer;
 }
