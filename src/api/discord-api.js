d52ad68e17285abd25130f59370d690a627eb002
Harvey Cambridge <49193507+BokChoyWarrior@users.noreply.github.com>
2023-02-18 17:16:07 +0000

    Implement eslint changes [add-eslint]

--- src/api/discord-api.js
+++ src/api/discord-api.js
@@ -2,16 +2,18 @@ import { DISCORD_API, GUILD_ID } from '../constants/env-constants.js';
 import { DiscordRequest } from './fetch.js';
 
 const USER_ENDPOINT = "users/";
+// eslint-disable-next-line no-unused-vars
 const GUILD_ENDPOINT = `/guilds/${GUILD_ID}/`;
 
 export async function getUserById(id) {
     return await DiscordRequest(`${DISCORD_API}${USER_ENDPOINT}${id}`, { method: 'GET' });
 }
 
+// eslint-disable-next-line no-unused-vars
 export async function getGuildUserById(id) {
     // return await DiscordRequest(`${DISCORD_API}${GUILD_ENDPOINT}members/${id}`, { method: 'GET' });
     // Temporary to not hit the rate limit
-    return await (new Promise((resolve, reject) => {
+    return await (new Promise((resolve) => {
         resolve({
             "avatar": null,
             "communication_disabled_until": null,
